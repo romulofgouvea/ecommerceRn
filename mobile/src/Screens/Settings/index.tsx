b8a5d17cb5a76d8d57c07a14a639909356a24476
Rômulo Fernandes Gouvea <romulo.gouvea.lemaf@gmail.com>
2019-10-16 10:14:30 -0300

    init settings

--- mobile/src/Screens/Settings/index.tsx
+++ mobile/src/Screens/Settings/index.tsx
@@ -1,21 +1,56 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Switch, Text } from "react-native";
 
-import { Container } from "./styles";
+import { Container, ContainerConfig, TitleCard, LineContainer } from "./styles";
+import { HeaderComponent } from "../../Components";
+
+import { Store } from "../../Services/SecureStore";
 
 function Settings({ navigation }) {
 
     //Variables
+    const [notification, setNotification] = useState(false);
 
     //Action Functions
+    function handleArrowBack() {
+        navigation.goBack(null);
+    }
+
+    async function getAllInStore() {
+        let not = await Store.getItem('notification');
+        if (not === null) {
+            await Store.setItem('notification', true);
+            not = true;
+        }
+        setNotification(not);
+    }
+
+    async function handleChangeValueNotifications() {
+        await Store.setItem('notification', !notification);
+        setNotification(!notification);
+
+    }
 
     //Lifecycle Functions
+    useEffect(() => {
+        getAllInStore();
+    }, [])
 
     //Render Functions
 
     return (
         <Container>
-            <Text>Settings</Text>
+            <HeaderComponent
+                iconLeft={{ name: "arrow-back", onPress: handleArrowBack }}
+                title="Configurações"
+            />
+            <ContainerConfig>
+                <TitleCard>Notificações</TitleCard>
+                <LineContainer>
+                    <Text>Ativar notificações</Text>
+                    <Switch onValueChange={handleChangeValueNotifications} value={notification} />
+                </LineContainer>
+            </ContainerConfig>
         </Container>
     );
 }
--- mobile/src/Screens/Settings/styles.ts
+++ mobile/src/Screens/Settings/styles.ts
@@ -2,7 +2,22 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex:1;
-    justify-content:center;
-    align-items: center;
     background-color: #F5F5F5;
+`;
+
+export const ContainerConfig = styled.View`
+    flex:1;
+    background-color: #F5F5F5;
+    padding: 16px;
+`;
+
+export const TitleCard = styled.Text`
+    color: #ACACAC;
+    padding: 0 0 8px 0;
+    font-size: 18px;
+`;
+
+export const LineContainer = styled.View`
+    flex-direction:row;
+    justify-content: space-between;
 `;
